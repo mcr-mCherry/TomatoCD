1d588c453b39913c46cf49c1a38222dfb948d1a3
Chenrui Mo <162422355+mcr-mCherry@users.noreply.github.com>
2026-07-14 22:20:04 +0800

    Update config.R

--- config.R
+++ config.R
@@ -12,12 +12,11 @@
 
 HUGGINGFACE_CONFIG <- list(
   repo_id = "TomatoCisRegDB/TomatoCisRegDB",
-  base_url = "",           # 保持为空，让 Shiny 自己根据当前域名处理
+  base_url = "",     
   use_huggingface = FALSE,
   timeout = 300
 )
 
-# 开启外部 URL 模式，这样 JBrowse2 和前端搜索才能通过 HTTP 访问数据
 USE_EXTERNAL_URL <- TRUE
 
 EXTERNAL_URLS <- list(
@@ -27,7 +26,6 @@ EXTERNAL_URLS <- list(
   gene_search = "gene_search"
 )
 
-# 除非你需要连接外部数据库下载数据，否则保持 FALSE 最稳
 USE_PROXY <- FALSE
 PROXY_HOST <- "127.0.0.1"
 PROXY_PORT <- 7897
